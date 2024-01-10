d788ea01a6c0ffc284246308854edd517aefe3a8
Abdul Rafay <99marafay@gmail.com>
2024-01-10 23:18:51 +0500

    Files Creatted

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class home extends StatefulWidget {
+  const home({super.key});
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+// ignore: camel_case_types
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/src/screens/splash_screen.dart
+++ lib/src/screens/splash_screen.dart
@@ -1,7 +1,14 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:simplethread/src/screens/home.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  static const String _animation =
+      "assets/animation/Black_white_animation.json";
 
   @override
   State<SplashScreen> createState() => SsplasScreenhState();
@@ -9,7 +16,27 @@ class SplashScreen extends StatefulWidget {
 
 class SsplasScreenhState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+          context,
+          PageTransition(
+            child: const home(),
+            type: PageTransitionType.fade,
+            duration: const Duration(seconds: 3, milliseconds: 30),
+          ));
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [Lottie.asset(SplashScreen._animation)],
+      ),
+    );
   }
 }
