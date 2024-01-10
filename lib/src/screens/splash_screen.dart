74e664eaa185969287d11c11653b1285b2039572
Abdul Rafay <99marafay@gmail.com>
2024-01-10 22:59:49 +0500

    Assets & Plugins

---
+++ lib/src/screens/splash_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => SsplasScreenhState();
+}
+
+class SsplasScreenhState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
